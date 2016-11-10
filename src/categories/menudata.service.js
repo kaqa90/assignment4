ef1ddc23f3bdaa760d077f53caf267d038328509
Khadija <khadija.k.qaraqe@gmail.com>
2016-11-10 15:10:16 +0200

    Updates on Module 4 - Restaurant Menu Items Categories

--- src/categories/menudata.service.js
+++ src/categories/menudata.service.js
@@ -5,8 +5,6 @@
 angular.module('data')
 .service('MenuDataService', MenuDataService);
 
-
-
   MenuDataService.$inject = ['$http'];
   function MenuDataService('$http') {
 
@@ -21,11 +19,10 @@ angular.module('data')
       return response;
     };
 
-    service.getItemsForCategory = function () {
+    service.getItemsForCategory = function (categoryShortName) {
       var response = $http({
         method: "GET",
         url: ("https://davids-restaurant.herokuapp.com/menu_items.json?category=" + categoryShortName)
-
       });
 
       return response;
