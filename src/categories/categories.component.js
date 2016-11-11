10e47e69825397a7d32d8793a3688f38bf379f31
Khadija <khadija.k.qaraqe@gmail.com>
2016-11-11 14:33:34 +0200

    Updates on Module 4 - Restaurant Menu Items Categories

--- src/categories/categories.component.js
+++ src/categories/categories.component.js
@@ -6,7 +6,7 @@ angular.module('MenuApp')
 .component('categories', {
   templateUrl: './src/categories/templates/categories.template.html',
   bindings: {
-    items: '<'
+    categories: '<'
   }
 });
 
