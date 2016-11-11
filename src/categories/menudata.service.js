f3d71618690ccb1f9c47ca80522724a651e5d2b9
Khadija <khadija.k.qaraqe@gmail.com>
2016-11-11 14:04:43 +0200

    Updates on Module 4 - Restaurant Menu Items Categories

--- src/categories/menudata.service.js
+++ src/categories/menudata.service.js
@@ -5,7 +5,7 @@ angular.module('data')
 .service('MenuDataService', MenuDataService);
 
   MenuDataService.$inject = ['$http'];
-  function MenuDataService('$http') {
+  function MenuDataService($http) {
     var service = this;
 
     service.getAllCategories = function () {
